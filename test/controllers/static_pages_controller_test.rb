fc0ffb1e7b86c0e4bb53fc8a2ab16c5b16f73a03
David Jarillo <david.jarillo@sontasmx.com>
2017-01-10 14:59:20 -0800

    added application helpers and created static pages of controller test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home' do
     get static_pages_home_url
     assert_response :success
-    assert_select 'title', 'Home | ' + @main_title
+    assert_select 'title', @main_title
   end
 
   test 'should get help' do
