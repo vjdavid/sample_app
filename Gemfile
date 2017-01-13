b54224aa7fcfb2ee08d9bdd4dc26dc648e78682e
David Jarillo <david.jarillo@sontasmx.com>
2017-01-13 12:44:16 -0800

    finish layout and routes

--- Gemfile
+++ Gemfile
@@ -8,10 +8,13 @@ gem 'coffee-rails', '4.2.1'
 gem 'jquery-rails', '4.1.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.4.1'
+gem 'bootstrap-sass', '3.3.6'
 
 group :development, :test do
   gem 'sqlite3', '1.3.12'
   gem 'byebug',  '9.0.0', platform: :mri
+  gem 'rails-controller-testing'
+  gem 'pry-rails'
 end
 
 group :development do
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,25 +12,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get home' do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select 'title', @main_title
   end
 
   test 'should get help' do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select 'title', 'Help | ' + @main_title
   end
 
   test 'should get about' do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select 'title', 'About | ' + @main_title
   end
 
   test 'should get contact' do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select 'title', 'Contact | ' + @main_title
   end
