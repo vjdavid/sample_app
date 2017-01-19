6176949bc57af1c4f67f10222ff26353b703501e
David Jarillo <david.jarillo@sontasmx.com>
2017-01-19 12:46:36 -0800

    implemented basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,16 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+  @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -33,6 +33,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
      end
      follow_redirect!
      assert_template 'users/show'
+     assert is_logged_in?
      assert_not flash[:danger]
    end
 
