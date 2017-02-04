5ace73cdd1f1e5636700bfe9fa7cb553f4c03cbf
David Jarillo <david.jarillo@sontasmx.com>
2017-02-03 17:43:04 -0800

    added the last changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -23,6 +24,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
     else
       render 'edit'
     end
@@ -35,6 +38,14 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Please log in.'
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
   end
 
 end
